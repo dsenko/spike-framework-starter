4aece987e9ad92e8c6703f9191284c4a430020cf
Dawid <dsenko26@gmail.com>
2017-03-21 21:22:30 +0100

    + fix

--- dist/libs/spike-framework-core/dist/spike-framework.js
+++ dist/libs/spike-framework-core/dist/spike-framework.js
@@ -515,10 +515,7 @@ app.system = {
      *
      **/
     __throwError: function (errorMessage, errorMessageBinding) {
-
-        console.log('throwing error ' + errorMessage);
-
-        throw  Error('Spike Framework: ' + app.util.System.bindStringParams(errorMessage, errorMessageBinding));
+        throw Error('Spike Framework: ' + app.util.System.bindStringParams(errorMessage, errorMessageBinding));
     },
 
     /**
@@ -1568,6 +1565,39 @@ app.router = {
      */
     redirect: function (path, pathParams, urlParams) {
         app.router.__redirectToView(path, pathParams, urlParams);
+    },
+
+    /**
+     * @public
+     *
+     * Opens given URL/URI using window.location or window.open
+     * if @redirectType provided
+     *
+     * @param url
+     * @param redirectType
+     */
+    location: function (url, redirectType) {
+
+        if(redirectType){
+
+            redirectType = redirectType.toLowerCase();
+
+            if(redirectType.indexOf('blank') > -1){
+                redirectType = '_blank';
+            }else if(redirectType.indexOf('self') > -1){
+                redirectType = '_self';
+            }else if(redirectType.indexOf('parent') > -1){
+                redirectType = '_parent';
+            }else if(redirectType.indexOf('top') > -1){
+                redirectType = '_top';
+            }
+
+            window.open(url, redirectType);
+
+        } else {
+            window.location = url;
+        }
+
     },
 
     /**
@@ -3614,221 +3644,225 @@ app.modal = {
  */
 app.partial = {
 
-    /**
-     * @private
-     * Storage for partial instances
-     */
-    __dataArchive: {},
+  /**
+   * @private
+   * Storage for partial instances
+   */
+  __dataArchive: {},
 
-    /**
-     * Function returns processed given partial template
-     * with passed data
-     *
-     * @param partial
-     * @param data
-     */
-    include: function (partial, model) {
+  /**
+   * Function returns processed given partial template
+   * with passed data
+   *
+   * @param partial
+   * @param data
+   */
+  include: function (partial, model) {
 
-        if (!partial) {
-            app.system.__throwError(app.system.__messages.PARITAL_INCLUDE_NOT_DEFINED, [partial.__name]);
-        }
+    if (!partial) {
+      app.system.__throwError(app.system.__messages.PARITAL_INCLUDE_NOT_DEFINED, [partial.__name]);
+    }
 
-        app.debug('Invoke app.partial.include with params: {0} ', [partial.__name]);
+    app.debug('Invoke app.partial.include with params: {0} ', [partial.__name]);
 
-        if (!model) {
-            model = {};
-        }
+    if (!model) {
+      model = {};
+    }
 
-        app.partial[partial.__name] = $.extend(true,  {}, app.partial.__dataArchive[partial.__name]);
+    app.partial[partial.__name] = $.extend(true, {}, app.partial.__dataArchive[partial.__name]);
 
 
-        app.debug('Returning partial {0} template ', [partial.__name]);
+    app.debug('Returning partial {0} template ', [partial.__name]);
 
-        return partial.__template($.extend(true,  partial, model));
-    },
+    return partial.__template($.extend(true, partial, model));
+  },
+
+  /**
+   * @public
+   *
+   * Substitute method for register
+   *
+   * @param partialName
+   */
+  add: function (partialName) {
+    this.register(partialName);
+  },
+
+  /**
+   * @public
+   *
+   * Registering new partial in application
+   * Creates partial object
+   *
+   * @param partialName
+   */
+  register: function (partialName, partialObject) {
+
+    // Filter if name is invalid (can break application)
+    app.system.__filterRestrictedNames(partialName);
+
+    if (partialObject.inherits) {
+      // Apply extending from abstracts
+      partialObject = app.abstract.__tryExtend(partialName, partialObject.inherits, partialObject);
+    }
+
+    app.log('Registering partial {0}', [partialName]);
+    app.debug('Invoke partial.register with params: {0} {1}', [partialName, partialObject]);
+
+    if (app.util.System.isNull(partialObject.replace)) {
+      partialObject.replace = false;
+    }
+
+    partialObject.__replace = partialObject.replace;
+
+    //Setting tyope of module
+    partialObject.__type = 'PARTIAL';
+
+    //Setting original name of module
+    partialObject.__name = partialName;
+
+    //Setting name starting from lower case , used with templates and directories names of partial
+    partialObject.__lowerCaseName = partialName.substring(0, 1).toLowerCase() + partialName.substring(1, partialName.length);
 
     /**
      * @public
      *
-     * Substitute method for register
-     *
-     * @param partialName
+     * Function that renders partial
+     * Creates new partial object based ond __dataArchive and assign reference to shortcut app.ctx
+     * Creates partial HTML template using @function @private __loadTemplate()
+     * Passing partial processed template to DOM element with @attr view attribute
+     * Rendering components used in rendered partial
+     * Invalidating existing modals
+     * Initializing partial with partialPassedData
+     * @param partialPassedData
      */
-    add: function (partialName) {
-        this.register(partialName);
-    },
+    partialObject.render = function (selector, model) {
+      app.debug('Invoke partialObject.__render');
 
-    /**
-     * @public
-     *
-     * Registering new partial in application
-     * Creates partial object
-     *
-     * @param partialName
-     */
-    register: function (partialName, partialObject) {
+      var __partialObject = $.extend(true, {}, app.partial.__dataArchive[partialObject.__name]);
 
-        // Filter if name is invalid (can break application)
-        app.system.__filterRestrictedNames(partialName);
+      if (!selector) {
+        app.system.__throwError(app.system.__messages.PARITAL_SELECTOR_NOT_DEFINED, [__partialObject.__name]);
+      }
 
-        if (partialObject.inherits) {
-            // Apply extending from abstracts
-            partialObject = app.abstract.__tryExtend(partialName, partialObject.inherits, partialObject);
+      __partialObject.rootSelector = selector;
+
+      var partialModel = $.extend(true, __partialObject, model);
+
+      if (__partialObject.before && app.util.System.isFunction(__partialObject.before)) {
+        app.debug('Invokes partial  {0} before() function', [__partialObject.__name]);
+        var returningModel = __partialObject.before(partialModel);
+
+        if (returningModel && returningModel.__name == __partialObject.__name) {
+          partialModel = returningModel;
         }
 
-        app.log('Registering partial {0}', [partialName]);
-        app.debug('Invoke partial.register with params: {0} {1}', [partialName, partialObject]);
+      }
 
-        if (app.util.System.isNull(partialObject.replace)) {
-            partialObject.replace = false;
-        }
+      app.debug('Binding partial {0} template to passed selector {1} ', [__partialObject.__name, selector]);
 
-        partialObject.__replace = partialObject.replace;
+      var renderedTemplate = __partialObject.__template(partialModel);
 
-        //Setting tyope of module
-        partialObject.__type = 'PARTIAL';
+      //Includes static templates
+      renderedTemplate = app.system.__replacePlainTemplates(renderedTemplate);
 
-        //Setting original name of module
-        partialObject.__name = partialName;
+      if (app.config.replaceLangKeys) {
+        renderedTemplate = app.message.__replaceTemplateKeys(renderedTemplate);
+      }
 
-        //Setting name starting from lower case , used with templates and directories names of partial
-        partialObject.__lowerCaseName = partialName.substring(0, 1).toLowerCase() + partialName.substring(1, partialName.length);
+      if (__partialObject.__replace) {
+        selector = app.partial.__replacePartial(selector, renderedTemplate);
+      } else {
+        selector.html(renderedTemplate);
+      }
 
-        /**
-         * @public
-         *
-         * Function that renders partial
-         * Creates new partial object based ond __dataArchive and assign reference to shortcut app.ctx
-         * Creates partial HTML template using @function @private __loadTemplate()
-         * Passing partial processed template to DOM element with @attr view attribute
-         * Rendering components used in rendered partial
-         * Invalidating existing modals
-         * Initializing partial with partialPassedData
-         * @param partialPassedData
-         */
-        partialObject.render = function (selector, model) {
-            app.debug('Invoke partialObject.__render');
+      if (__partialObject.after && app.util.System.isFunction(__partialObject.after)) {
+        app.debug('Invokes partial  {0} after() function', [__partialObject.__name]);
+        __partialObject.after(partialModel, __partialObject.rootSelector);
+      }
 
-            var __partialObject = $.extend(true,  {}, app.partial.__dataArchive[partialObject.__name]);
+      //Binds spike events
+      app.system.__bindEvents(selector);
 
-            if (!selector) {
-                app.system.__throwError(app.system.__messages.PARITAL_SELECTOR_NOT_DEFINED, [__partialObject.__name]);
-            }
+      //Translate DOM
+      app.message.__translate();
 
-          __partialObject.rootSelector = selector;
-
-            var partialModel = $.extend(true,  __partialObject, model);
-
-            if (__partialObject.before && app.util.System.isFunction(__partialObject.before)) {
-                app.debug('Invokes partial  {0} before() function', [__partialObject.__name]);
-                var returningModel = __partialObject.before(partialModel);
-
-                if(returningModel && returningModel.__name == __partialObject.__name){
-                    partialModel = returningModel;
-                }
-
-            }
-
-            app.debug('Binding partial {0} template to passed selector {1} ', [__partialObject.__name, selector]);
-
-            var renderedTemplate = __partialObject.__template(partialModel);
-
-            //Includes static templates
-            renderedTemplate = app.system.__replacePlainTemplates(renderedTemplate);
-
-            if (app.config.replaceLangKeys) {
-                renderedTemplate = app.message.__replaceTemplateKeys(renderedTemplate);
-            }
-
-            if (__partialObject.__replace) {
-                selector = app.partial.__replacePartial(selector, renderedTemplate);
-            } else {
-                selector.html(renderedTemplate);
-            }
-
-
-            //Binds spike events
-            app.system.__bindEvents(selector);
-
-            //Translate DOM
-            app.message.__translate();
-
-        };
-
-        /**
-         * @private
-         *
-         * Creating path to partial view HTML file
-         * Creates path based on html2js template file and directories structure
-         * @param partialObject
-         */
-        partialObject.__createPartialViewPath = function (partialObject) {
-            app.debug('Invoke partialObject.__createPartialViewPath with params: {0}', [partialObject]);
-
-            partialObject.__view = app.config.rootPath + "/" + app.config.partialDirectory + "/" + partialObject.__lowerCaseName + "/" + partialObject.__lowerCaseName + ".partial.html"
-
-        }
-
-        /**
-         * @private
-         *
-         * Function retrieving partial's template from global window[templates] variable based on generated view path
-         * If template not specified, throw Error
-         * Creating dynamic selectors binded to template using @private system.__createSelectors()
-         *
-         */
-        partialObject.__loadTemplate = function () {
-            app.debug('Invoke partialObject.__loadTemplate');
-
-            var templateFunction = window[app.__globalTemplates][partialObject.__view];
-
-            if (!templateFunction) {
-                app.system.__throwError('No view found for partial: {0}, view path: {1}', [partialObject.__name, partialObject.__view]);
-            }
-
-            partialObject.__template = templateFunction;
-
-        }
-
-        //Creating view path once per application initialization
-        partialObject.__createPartialViewPath(partialObject);
-
-        //Sets partial template
-        partialObject.__loadTemplate();
-
-        //Creating copy of partial object in @private __dataArchive and in partial[partialName]
-        app.partial.__dataArchive[partialName] = $.extend(true,  {}, partialObject);
-        app.partial[partialName] = $.extend(true,  {}, partialObject);
-
-    },
+    };
 
     /**
      * @private
      *
-     * Function replaces partial selector with given template
-     * and migrate partial selector @id attribute into
-     * root element of given template
-     *
-     * @param selector
-     * @param templateHtml
-     *
+     * Creating path to partial view HTML file
+     * Creates path based on html2js template file and directories structure
+     * @param partialObject
      */
-    __replacePartial: function (selector, templateHtml) {
+    partialObject.__createPartialViewPath = function (partialObject) {
+      app.debug('Invoke partialObject.__createPartialViewPath with params: {0}', [partialObject]);
 
-        var selectorId = selector.attr('id');
-
-        var rootElementPart = templateHtml.substring(0, templateHtml.indexOf('>'))
-        rootElementPart += ' id="' + selectorId + '" ';
-
-        templateHtml = rootElementPart + templateHtml.substring(templateHtml.indexOf('>'), templateHtml.length);
-
-        selector.replaceWith(templateHtml);
-
-        app.system.__clearSelectorInCache(selectorId);
-
-        return $('#'+selectorId);
+      partialObject.__view = app.config.rootPath + "/" + app.config.partialDirectory + "/" + partialObject.__lowerCaseName + "/" + partialObject.__lowerCaseName + ".partial.html"
 
     }
+
+    /**
+     * @private
+     *
+     * Function retrieving partial's template from global window[templates] variable based on generated view path
+     * If template not specified, throw Error
+     * Creating dynamic selectors binded to template using @private system.__createSelectors()
+     *
+     */
+    partialObject.__loadTemplate = function () {
+      app.debug('Invoke partialObject.__loadTemplate');
+
+      var templateFunction = window[app.__globalTemplates][partialObject.__view];
+
+      if (!templateFunction) {
+        app.system.__throwError('No view found for partial: {0}, view path: {1}', [partialObject.__name, partialObject.__view]);
+      }
+
+      partialObject.__template = templateFunction;
+
+    }
+
+    //Creating view path once per application initialization
+    partialObject.__createPartialViewPath(partialObject);
+
+    //Sets partial template
+    partialObject.__loadTemplate();
+
+    //Creating copy of partial object in @private __dataArchive and in partial[partialName]
+    app.partial.__dataArchive[partialName] = $.extend(true, {}, partialObject);
+    app.partial[partialName] = $.extend(true, {}, partialObject);
+
+  },
+
+  /**
+   * @private
+   *
+   * Function replaces partial selector with given template
+   * and migrate partial selector @id attribute into
+   * root element of given template
+   *
+   * @param selector
+   * @param templateHtml
+   *
+   */
+  __replacePartial: function (selector, templateHtml) {
+
+    var selectorId = selector.attr('id');
+
+    var rootElementPart = templateHtml.substring(0, templateHtml.indexOf('>'))
+    rootElementPart += ' id="' + selectorId + '" ';
+
+    templateHtml = rootElementPart + templateHtml.substring(templateHtml.indexOf('>'), templateHtml.length);
+
+    selector.replaceWith(templateHtml);
+
+    app.system.__clearSelectorInCache(selectorId);
+
+    return $('#' + selectorId);
+
+  }
 
 
 };/**
@@ -4405,9 +4439,9 @@ app.util = {
          */
         bindStringParams: function (string, objectOrArrayParams) {
 
-            if (!objectOrArrayParams) {
-                return string;
-            }
+           if(string.indexOf('{') == -1 || objectOrArrayParams.toString().indexOf('[object Object]') > -1 || !objectOrArrayParams){
+             return string;
+           }
 
             if (objectOrArrayParams instanceof Array) {
 
